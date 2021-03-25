7e02d3f90b3246c10c1062cdfc733f8cba0df49e
MONA <mona.stingl@hs-furtwangen.de>
2021-03-25 12:21:01 +0100

    RandomPoem_finished
    Inklusive Kommentare

---
+++ Zufallsgedicht/TypeScript.js
@@ -0,0 +1,26 @@
+"use strict";
+var RandomPoem;
+(function (RandomPoem) {
+    // Drei Arrays mit den jeweiligen Subjektiven, Verben und Objektiven
+    let subjects = ["Paul", "Lisa", "Max", "Sophie", "Tom", "Die Putzfrau"];
+    let verbs = ["tötet", "liebt", "putzt", "hasst", "heiratet", "verbrennt"];
+    let objects = ["Fliegen", "seinen/ihren Partner", "eine Giraffe", "das Haus", "die Sekretärin", "die Polizei"];
+    // While-Schleife, die so lange ausgeführt wird, solange sich noch Subjektive im jeweiligen Array befinden.
+    // Mit document.write manipuliere ich den Dom und bereite ihn auf die Verse des Gedichts vor.
+    while (0 < subjects.length) {
+        let verse = getVerse();
+        console.log(verse);
+        document.write("<p>" + verse + "</p>");
+    }
+    // Funktion die das Gedicht mit den zufälligen Versen ausführt.
+    function getVerse() {
+        return popRandomElement(subjects) + " " + popRandomElement(verbs) + " " + popRandomElement(objects);
+    }
+    // Funktion, die durch Math.random eine Zufallszahl generiert, die dann als Index für alle drei Arrays verwendet wird.
+    // Mit Splice cutte ich das Wort weg, welches schon verwendet wurde, damit es nicht doppelt vorkommen kann.
+    function popRandomElement(_array) {
+        let index = Math.floor(Math.random() * _array.length);
+        return _array.splice(index, 1).toString();
+    }
+})(RandomPoem || (RandomPoem = {}));
+//# sourceMappingURL=TypeScript.js.map
---
+++ Zufallsgedicht/TypeScript.ts
@@ -0,0 +1,26 @@
+namespace RandomPoem {
+
+    // Drei Arrays mit den jeweiligen Subjektiven, Verben und Objektiven
+    let subjects: string[] = ["Paul", "Lisa", "Max", "Sophie", "Tom", "Die Putzfrau"];
+    let verbs: string[] = ["tötet", "liebt", "putzt", "hasst", "heiratet", "verbrennt"];
+    let objects: string[] = ["Fliegen", "seinen/ihren Partner", "eine Giraffe", "das Haus", "die Sekretärin", "die Polizei"];
+    
+    // While-Schleife, die so lange ausgeführt wird, solange sich noch Subjektive im jeweiligen Array befinden.
+    // Mit document.write manipuliere ich den Dom und bereite ihn auf die Verse des Gedichts vor.
+    while (0 < subjects.length) {
+        let verse: string = getVerse();
+        console.log(verse);
+        document.write("<p>" + verse + "</p>");
+    } 
+
+    // Funktion die das Gedicht mit den zufälligen Versen ausführt.
+    function getVerse(): string {
+        return popRandomElement(subjects) + " " + popRandomElement(verbs) + " " + popRandomElement(objects);
+    }
+    // Funktion, die durch Math.random eine Zufallszahl generiert, die dann als Index für alle drei Arrays verwendet wird.
+    // Mit Splice cutte ich das Wort weg, welches schon verwendet wurde, damit es nicht doppelt vorkommen kann.
+    function popRandomElement(_array: string[]): string {
+        let index: number = Math.floor(Math.random() * _array.length);
+        return _array.splice(index, 1).toString();
+    }
+}
